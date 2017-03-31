5d06e87494e6e31a68741131054bafe9cc4b56dc
Ashok kumar <ashok.kumar@ashoks-MacBook-Pro-2.local>
2017-03-31 11:44:09 +0530

    Used library in the app.

--- app/src/main/java/com/ashokslsk/checknet/MainActivity.java
+++ app/src/main/java/com/ashokslsk/checknet/MainActivity.java
@@ -1,13 +1,37 @@
 package com.ashokslsk.checknet;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.ashokslsk.checknetwork.NetworkStateReceiver;
+
+public class MainActivity extends AppCompatActivity implements NetworkStateReceiver.NetworkStateReceiverListener{
+
+    //Network instance
+    private NetworkStateReceiver networkStateReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialise the libe
+        //Network register
+        networkStateReceiver = new NetworkStateReceiver();
+        networkStateReceiver.addListener(this);
+        this.registerReceiver(networkStateReceiver, new IntentFilter(android.net.ConnectivityManager.CONNECTIVITY_ACTION));
+
+
+    }
+
+    @Override
+    public void networkAvailable() {
+
+    }
+
+    @Override
+    public void networkUnavailable() {
+
     }
 }
